646fce0beaeba6d30251611b66e2bb7eccc6c0e2
llh <850360626@qq.com>
2017-04-28 11:31:51 +0800

    add something ,

---
+++ app/src/main/java/io/ipcdemo/mvp/model/User.java
@@ -0,0 +1,44 @@
+package io.ipcdemo.mvp.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by liuliuh on 2017/4/28.
+ */
+
+public class User implements Serializable {
+    private String mName;
+    private int mAge;
+    private String mSex;
+
+    public String getmName() {
+        return mName;
+    }
+
+    public int getmAge() {
+        return mAge;
+    }
+
+    public String getmSex() {
+        return mSex;
+    }
+
+    public void setmName(String mName) {
+        this.mName = mName;
+    }
+
+    public void setmAge(int mAge) {
+        this.mAge = mAge;
+    }
+
+    public void setmSex(String mSex) {
+        this.mSex = mSex;
+    }
+
+    public User(String mName, int mAge, String mSex) {
+        this.mName = mName;
+        this.mAge = mAge;
+        this.mSex = mSex;
+    }
+
+}
